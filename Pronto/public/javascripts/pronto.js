977933621e063be5e8d863a64491b5b0439f1a1e
francoismarchal9352 <f.marchal@students.ephec.be>
2016-10-25 00:10:59 +0200

    interface client - beta

--- Pronto/public/javascripts/pronto.js
+++ Pronto/public/javascripts/pronto.js
@@ -2,6 +2,21 @@
  * Created by Alex on 11/10/2016.
  */
 
+// fonctions utilitaires
+function setElem(id, v){
+    // place la valeur v dand l'élément d'identifiant id
+    document.getElementById(id).innerHTML = v;
+}
+function addElem(id, v){
+    // ajoute la valeur v à l'élément d'identifiant id
+    document.getElementById(id).innerHTML += v;
+}
+function getElem(id){
+    // renvoie le contenu de l'élément d'identifiant id
+    return document.getElementById(id).innerHTML;
+}
+
+
 
 var cart = [];
 var tmp = [];
@@ -30,8 +45,7 @@ function getItem(a) {
     item = document.getElementById(a + 'Nom').textContent;
     add(a,item);
     console.log(tmp);
-    temp = document.getElementById("temp");
-    temp.textContent = tmp;
+    drawCommand();
 }
 
 
@@ -42,22 +56,72 @@ function add(val1, val2){
 
 function addQuant(val1,val2){
     tmp.push(val1);
-
+    addCart(val2)
 }
 
-function addCart(){
-    cart.push({ commande : tmp});
+function addCart(val){
+    cart.push({ val : tmp});
     tmp = [];
 }
 
-function submit() {
-    addCart();
+function onSubmit() {
     $.ajax({
         type: 'POST',
         data: JSON.stringify(cart),
         contentType: 'application/json',
         url: '/process_post'
     });
-    location.reload();
-    return false;
+    $(document).ready(function(){
+        $('.btn-success').popover({trigger: "focus", content: "commande envoyée", placement: "bottom"});
+    });
 }
+
+//fonction qui affiche le résumé de la commande
+function drawCommand(){
+     setElem('temp', '');
+     for (var i = 0, c = tmp.length; i < c; i+=2) {
+         st = '<div class=thumbnail id=\"'
+             + tmp[i+1]
+             + ' \"><div class=\"caption-full\"><h4><a href=\"#\">'
+             + tmp[i]
+             + '</a></h4></div></div>';
+         addElem('temp',st);
+     }
+}
+
+function affiche(){
+    $(document).ready(function(){
+        $('.link_ingredient').tooltip({placement: "auto top", toggle: "tooltip", title: "<h4>Ingrédients:</h4> jumbo lump crab, avocado, herb oil.", animation:"true", html: "true"});
+    });
+}
+
+ //rajouter dans ma fonction submit
+
+//partie js pour incrémenter et décrémenter la quantité
+var res = document.getElementById('result');
+result = parseInt(res.value,10);
+
+var plus = document.getElementById('plus');
+var moins = document.getElementById('moins');
+
+// prendre en compte la modification du nombre au clavier
+res.addEventListener('blur', function() {
+    result = document.getElementById('result');
+    result = parseInt(result.value,10);
+});
+
+// boutton +
+plus.addEventListener('click', function() {
+    if(result >= 0 && result < 99){
+        result++;
+        document.getElementById('result').value= result;
+    }
+});
+
+// boutton -
+moins.addEventListener('click', function() {
+    if(result > 0 && result <= 99){
+        result--;
+        document.getElementById('result').value= result;
+    }
+});
